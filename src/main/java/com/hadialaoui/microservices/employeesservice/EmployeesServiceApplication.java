81645bc5a176be4de6639774b52765615ebeb183
hadialaoui <a.hadialaoui@gmail.com>
2019-10-29 00:08:49 +0100

    connecting employess microservice to Eureka

--- src/main/java/com/hadialaoui/microservices/employeesservice/EmployeesServiceApplication.java
+++ src/main/java/com/hadialaoui/microservices/employeesservice/EmployeesServiceApplication.java
@@ -2,10 +2,12 @@ package com.hadialaoui.microservices.employeesservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication(scanBasePackages="com.hadialaoui.microservices.employeesservice.*")
 @EnableFeignClients
+@EnableDiscoveryClient
 public class EmployeesServiceApplication {
 
 	public static void main(String[] args) {
